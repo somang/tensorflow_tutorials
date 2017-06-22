8442b78298b161736585f92a6eff8eea4e885c89
somang <sixjuwel@gmail.com>
2017-06-22 15:34:29 -0400

    d1done

--- tensor2/mnist_softmax.py
+++ tensor2/mnist_softmax.py
@@ -1,3 +1,7 @@
+# Modified National Institute of Standards and Technology database
+# is a large database of handwritten digits that is commonly 
+# used for training various image processing systems.
+
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
@@ -7,3 +11,10 @@ import numpy as np
 
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+# This is a classic case where a softmax regression is a natural, simple model. 
+# If you want to assign probabilities to an object being one of several different things, 
+# softmax is the thing to do, because
+# softmax gives us a list of values between 0 and 1 that add up to 1. 
+# Even later on, when we train more sophisticated models,
+# the final step will be a layer of softmax.
